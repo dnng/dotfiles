c34941d0510c66a3dce2a0dbf71b8acea8febbd5
Daniel Noguchi <danielnoguchi@gmail.com>
2018-02-24 01:01:54 -0300

    Update provision script

--- provision.sh
+++ provision.sh
@@ -9,4 +9,9 @@ cp .vimperatorrc ~/
 cp .vimrc ~/
 cp .zshrc ~/ 
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+# From https://github.com/junegunn/vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# create plugin dir if not exists
+mkdir ~/.vim/plugged
