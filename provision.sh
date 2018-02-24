23a853b333e64796f92a1d67323d90c82b30a557
Daniel Noguchi <danielnoguchi@gmail.com>
2018-02-24 01:13:07 -0300

    Add file checking to provision script

--- provision.sh
+++ provision.sh
@@ -3,15 +3,16 @@ cp .bash_aliases ~/
 cp .bashrc ~/
 cp .gitignore ~/
 cp .gitconfig ~/
-cp .i3/ ~/
 cp .screenrc ~/
 cp .vimperatorrc ~/
 cp .vimrc ~/
 cp .zshrc ~/ 
+cp -r .i3/ ~/
 
 # From https://github.com/junegunn/vim-plug
+[ -f ~/.vim/autoload/plug.vim ] || \
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # create plugin dir if not exists
-mkdir ~/.vim/plugged
+[ -d ~/.vim/plugged ] || mkdir ~/.vim/plugged
